d833f0e108e0132cd927dedf456fae27345a2688
Santiago Cendales <srcendales94@gmail.com>
2020-02-29 00:55:35 -0500

    Added new releases to homepage

--- src/app/core/spotify.service.ts
+++ src/app/core/spotify.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,8 @@ export class SpotifyService {
   constructor(
     private httpClient: HttpClient
   ) { }
+
+  // purabsdjbakdbas = [];
 
   clientId = '978b3f7580f143f9ba41c41489abc026';
   clientSecret = '04dad104d4e3442098c4aef22ff25439';
@@ -21,15 +24,20 @@ export class SpotifyService {
       Authorization: 'Basic ' + btoa(this.clientId + ':' + this.clientSecret),
       'Content-type': 'application/x-www-form-urlencoded'
     };
+
     const body = new HttpParams()
     .set('grant_type', 'client_credentials');
-    return this.httpClient.post('https://accounts.spotify.com/api/token', body, { headers })
-    .subscribe(dataToken => {
-      this.requestResponse = dataToken;
-      this.token = this.requestResponse.access_token;
-      console.log(this.token);
-    },
-      err => alert('Invalid Client Credentials'));
-    }
+
+    return this.httpClient.post('https://accounts.spotify.com/api/token', body, { headers });
+  }
+
+  getNewReleases() {
+
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer BQB1iOqlvOJNZDbNPdmr9ja4v_KYQSlllhKqaivYpj37zTQEjcFadOAE1ktKwyU75B958HB_XBF6zYu89jU'
+    });
+    return this.httpClient.get('https://api.spotify.com/v1/browse/new-releases?country=CO&limit=24&offset=0', { headers }).pipe(
+      map(data => data['albums'].items));
+  }
 
 }
